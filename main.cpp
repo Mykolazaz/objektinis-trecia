905f9543a623811983ff7072ff8abe5fe9cd43b3
Mykolazaz <mykolas.motiejunas@gmail.com>
2024-09-13 15:18:17 +0300

    Sukonstruota paprasta dalinė sistema duomenų įvedimui

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 
-struct studentas{
+struct Studentas{
     string vardas;
     string pavarde;
     int tarpRez[15];
     int egzamRez;
 };
 
-int main(){}
+
+int main(){
+    vector<Studentas> studentai;
+    int studSk;
+
+    cout << "Apie kiek studentų pildysite informaciją?:" << endl;
+    cin >> studSk;
+    studentai.resize(studSk);
+
+    for (int i=0; i < studSk; i++){
+        cout << "Įveskite studento vardą, pavardę ir egz. įvertinimą: " << endl;
+        cin >> studentai[i].vardas >> studentai[i].pavarde >> studentai[i].egzamRez;
+    }
+
+    for (int i=0; i < studSk; i++){
+        cout << studentai[i].vardas << studentai[i].pavarde << studentai[i].egzamRez << endl;
+    }
+
+    return 0;
+}
