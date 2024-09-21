76ec6e6d2e4a991c123b2c9065c5db78e19ab2d5
Mykolazaz <mykolas.motiejunas@gmail.com>
2024-09-21 17:58:13 +0300

    Implementuotas n tarpinių rezultatų saugojimas vektoriuje, panaikintas universalus "std" naudojimas

--- main.cpp
+++ main.cpp
@@ -1,31 +1,44 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <iomanip>
 
 
 struct Studentas{
-    string vardas;
-    string pavarde;
-    int tarpRez[15];
+    std::string vardas;
+    std::string pavarde;
+    int tarpRezSk;
+    std::vector<int> tarpRez;
     int egzamRez;
 };
 
 
 int main(){
-    vector<Studentas> studentai;
+    std::vector<Studentas> studentai;
     int studSk;
 
-    cout << "Apie kiek studentų pildysite informaciją?:" << endl;
-    cin >> studSk;
+    std::cout << "Apie kiek studentų pildysite informaciją?:" << std::endl;
+    std::cin >> studSk;
     studentai.resize(studSk);
 
     for (int i=0; i < studSk; i++){
-        cout << "Įveskite studento vardą, pavardę ir egz. įvertinimą: " << endl;
-        cin >> studentai[i].vardas >> studentai[i].pavarde >> studentai[i].egzamRez;
+        int input;
+
+        std::cout << "Įveskite studento vardą, pavardę, tarpinių rezultatų sk. ir egz. įvertinimą: " << std::endl;
+        std::cin >> studentai[i].vardas >> studentai[i].pavarde >> studentai[i].tarpRezSk >> studentai[i].egzamRez;
+        std::cout << "Dabar įveskite šio studento tarpinių įvetinimų rezultatus per 'space' (-1 sustabdo vedimą): " << std::endl;
+        
+        while(std::cin >> input) { 
+            if(input < 0) { 
+                break; 
+            } 
+ 
+        studentai[i].tarpRez.push_back(input); 
+        
+        }
     }
 
     for (int i=0; i < studSk; i++){
-        cout << studentai[i].vardas << studentai[i].pavarde << studentai[i].egzamRez << endl;
+        std::cout << studentai[i].vardas << studentai[i].pavarde << studentai[i].egzamRez << studentai[i].tarpRez.size() << std::endl;
     }
 
     return 0;
