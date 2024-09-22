8c7be013631351706f3c1d092df2ced291677c3e
Mykolazaz <mykolas.motiejunas@gmail.com>
2024-09-22 14:29:15 +0300

    Sutvarkytas teksto lygiavimas "kursiokai.txt" faile

--- studentas.cpp
+++ studentas.cpp
@@ -85,22 +85,21 @@ void generate(int studGenSk, int ndGenSk){
 
     studGenSk += 1;
     ndGenSk += 1;
-    int ndIvertSk = ndGenSk + 1;
 
     fw << std::left << std::setw(15) << std::setfill(' ') << "Pavardė"
     << std::setw(15) << std::setfill(' ') << "Vardas";
     for (int i = 1; i < ndGenSk; i++){
-        fw << "ND" << i << std::setw(3) << " ";
+        fw << "ND" << std::setw(2) << std::left << i << " ";
     }
-    fw << std::setw(20) << "Egzaminimas" << std::endl;
+    fw << std::setw(10) << "Egzaminimas" << std::endl;
     
     for (int i = 1; i < studGenSk; i++){
         fw << std::left << std::setw(15) << ("Pavardė"+std::to_string(i))
         << std::setw(15) << ("Vardas"+std::to_string(i));
-        for (int j = 1; j < ndIvertSk; j++){
+        for (int j = 1; j < ndGenSk; j++){
             fw << std::setw(5) << rand() % 10 + 1;    
         }
-        fw << std::setw(15) << rand() % 10 + 1 << std::endl;
+        fw << std::setw(10) << rand() % 10 + 1 << std::endl;
     }
     fw.close();
 }
